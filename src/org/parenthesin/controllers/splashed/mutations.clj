444a5aea04f17f76f1eb1a5b199cceab0a14f262
Rafael Delboni <rafadelboni@gmail.com>
2020-01-17 10:43:16 -0300

    fix: New mutation name

--- src/org/parenthesin/controllers/splashed/mutations.clj
+++ src/org/parenthesin/controllers/splashed/mutations.clj
@@ -8,8 +8,8 @@
   [env {list-id   :list/id
         person-id :person/id}]
   ;; optional, this is how you override what symbol it responds to. Defaults to current ns.
-  {::pc/sym `splashed/delete-person}
-  (log/info :msg (str "Deleting person" person-id "from list" list-id))
+  {::pc/sym `app.mutations/delete-person}
+  (log/info :msg (str "Deleting person " person-id " from list " list-id))
   (swap! list-table
          update
          list-id
